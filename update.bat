7202810651fc7e8f44c31222960e75c956d62b5b
dymaxionkim <dymaxion.kim@gmail.com>
2023-07-15 11:05:39 +0900

    Update - conda and pip

--- update.bat
+++ update.bat
@@ -8,7 +8,12 @@ call scoop cache rm *
 
 REM conda
 call %userprofile%\scoop\apps\miniconda3\current\Scripts\activate.bat
+call conda install -y brotlipy urllib3 pandas openpyxl psutil numpy matplotlib ezdxf pysimplegui img2pdf
 call conda update --all
+
+REM pip
+call yes | pip install pdf2pptx PyMuPDF pySW
+copy /Y D:/github/xlsx2sw\patch\commSW.py %userprofile%\scoop\apps\miniconda3\current\Lib\site-packages\pySW\commSW.py
 
 echo Finished.
 exit
